f5745e5ae4ae19adae5d532a711615dcee0cf20f
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2025-02-09 16:50:22 +0200

    added snackbar page, logic and styles

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,64 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+class promiseGenerator {
+  constructor() {
+    this.form = document.querySelector('.form');
+    this.bindEvents();
+  }
+  formSubmit(e) {
+    e.preventDefault();
+    const formData = this.form.elements;
+    const delayValue = parseInt(formData.delay.value, 10);
+    const stateValue = formData.state.value;
+
+    this.generatePromise(delayValue, stateValue)
+      .then(delay => {
+        iziToast.success({
+          title: '✅',
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+          icon: '',
+          progressBar: false,
+          timeout: 8000000,
+          messageColor: 'white',
+          backgroundColor: '#56c985',
+          messageSize: '24',
+          titleSize: '24',
+          close: false,
+          animateInside: false,
+          transitionIn: 'fadeIn',
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          title: '❌',
+          message: `Rejected promise in ${delay}ms`,
+          position: 'topRight',
+          icon: '',
+          progressBar: false,
+          timeout: 80000000,
+          backgroundColor: '#fb6969',
+          messageColor: 'white',
+          messageSize: '24',
+          titleSize: '24',
+          close: false,
+          animateInside: false,
+          transitionIn: 'fadeIn',
+        });
+      })
+      .finally(() => this.form.reset());
+  }
+  generatePromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        state === 'fulfilled' ? resolve(delay) : reject(delay);
+      }, delay);
+    });
+  }
+  bindEvents() {
+    this.form.addEventListener('submit', this.formSubmit.bind(this));
+  }
+}
+
+new promiseGenerator();
